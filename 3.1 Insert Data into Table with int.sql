58fb78597f89f98c62e3be045cf21a26abdfedb1
Mayank Agarwal <mayankagarwal4348@gmail.com>
2026-04-04 01:40:50 +0530

    Add files via upload

---
+++ 3.1 Insert Data into Table with int.sql
@@ -0,0 +1,11 @@
+SELECT * FROM employee2;
+
+INSERT INTO employee2(employee_id, employee_name, position, department, hire_date, salary)
+		VALUES (101, 'Mayank Agarwal', 'Data Analyst', 'Data Science', '2026-05-01', 68425.00),
+			(102, 'Priya Desai', 'Develeper', 'Development', '2024-05-20', 75626.00),
+			(103, 'Sakshi sharma', 'Quality Trainee', 'Testing', '2025-09-13', 45000.00),
+			(104, 'Saurabh Sukla', 'Marketing Head', 'Sales', '2024-02-28', 65473.00),
+			(105, 'Navdeep Pal', 'Data Engineer', 'Data Science', '2025-07-15', 50000.00),
+			(106, 'Rudra Mishra', 'Resourse Manager', 'HR', '2022-04-04', 30500.00),
+			(107, 'Ankit Raj', 'Training trainee', 'Testing', '2023-01-20', 45000.00),
+			(108, 'Khushi', 'Sales Marketing', 'Sales', '2026-06-20', 25000.00);
